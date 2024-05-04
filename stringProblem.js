7db97b4b3f489b3f7c71f1c5fbbd1f910ae95de5
kamal Jyotwal <kkjyotwal100@gmail.com>
2024-05-05 02:25:33 +0530

    file restruc.

--- stringProblem.js
+++ stringProblem.js
@@ -1,26 +1,29 @@
-//returning the elements with the maximum string length
+//Return the strings with most character
 function allLongestStrings(inputArray) {
-  // var result = [];
-  // let prevCount = 0;
-  // inputArray.map((i) => {
-  //   if (i.length >= prevCount) {
-  //     prevCount = i.length;
-  //     result.push(i);
-  //   }
-  // });
-  // const newres = result.filter((k) => k.length >= prevCount);
-  // return newres;
-  // ----------------------------
-  // Alternate 1
-  // let a = inputArray.sort((a, b) => b.length - a.length);
-  // let maxLength = a[0].length;
-  // let b = a.filter((str) => str.length == maxLength);
-  // console.log(b);
-  // ----------------------------
-  // Alternate 2
   let maxLength = Math.max(...inputArray.map((str) => str.length));
   let b = inputArray.filter((str) => str.length === maxLength);
   console.log(b);
 }
-
 console.log(allLongestStrings(["abc", "eeee", "abcd", "dcd"]));
+
+// Approach 2----------------------------------------
+// function allLongestStrings(inputArray) {
+//   let a = inputArray.sort((a, b) => b.length - a.length);
+//   let maxLength = a[0].length;
+//   let b = a.filter((str) => str.length == maxLength);
+//   console.log(b);
+// }
+
+// Approach 3----------------------------------------
+// function allLongestStrings(inputArray) {
+//   var result = [];
+//   let prevCount = 0;
+//   inputArray.map((i) => {
+//     if (i.length >= prevCount) {
+//       prevCount = i.length;
+//       result.push(i);
+//     }
+//   });
+//   const newres = result.filter((k) => k.length >= prevCount);
+//   return newres;
+// }
