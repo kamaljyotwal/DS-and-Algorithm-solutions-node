425d535505cb606bd553462ffceba1b67a7e9f46
kamal Jyotwal <kkjyotwal100@gmail.com>
2024-05-05 04:33:02 +0530

    rewritten with other approaches and optimized prev code

--- rotateImage.js
+++ rotateImage.js
@@ -1,17 +1,13 @@
-function rotateImage(a) {
-  var newArr = [];
-  var inner = [];
-
-  for (let i = 0; i < a.length; i++) {
-    inner = [];
-    for (let j = 0; j < a.length; j++) {
-      inner.push(a[j][i]);
+// Best Approach------------------------------------------------------
+function rotateImage(arr) {
+  let a = new Array(arr.length).fill().map(() => []);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      a[j].unshift(arr[i][j]);
     }
-    newArr.push(inner.reverse());
   }
-  return newArr;
+  return a;
 }
-
 console.log(
   rotateImage([
     [1, 2, 3],
@@ -19,3 +15,22 @@ console.log(
     [7, 8, 9],
   ])
 );
+
+// Approach 2------------------------------------------------------
+// function rotateImage(a) {
+//   let final = [];
+//   for (let i = 0; i < a.length; i++) {
+//     let inner = [];
+//     for (let j = 0; j < a.length; j++) {
+//       inner.push(a[j][i]);
+//     }
+//     console.log(inner);
+//     final.push(inner.reverse());
+//   }
+//   return final;
+// }
+
+// Approach 3-----------------------------------------------------
+// function rotateImage(a) {
+//   return a.map((row, rowIndex) => row.map((val, colIndex) => a[a.length - 1 - colIndex][rowIndex]));
+// }
